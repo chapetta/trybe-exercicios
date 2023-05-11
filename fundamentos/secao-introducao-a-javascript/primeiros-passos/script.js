056f51c21e730b322c0dbdd2611b0ddfed1e63e7
chapetta <cha.petta@hotmail.com>
2023-05-11 19:33:38 -0300

    exercicios do dia

--- fundamentos/secao-introducao-a-javascript/primeiros-passos/script.js
+++ fundamentos/secao-introducao-a-javascript/primeiros-passos/script.js
@@ -9,3 +9,47 @@ else if (nota < 80 && nota > 60 ) {
 else {
   console.log('infelizmente voce reprovou')
 }
+
+let currentHour = 8
+let message = ''
+
+if (currentHour >= 22) {
+  message = 'Não deveríamos comer nada, é hora de dormir'
+}
+else if ( currentHour >= 18 && currentHour <= 22) {
+  message = 'Rango da noite, vamos jantar :D'
+}
+else if (currentHour >= 14 && currentHour <= 18) {
+  message = 'Vamos fazer um bolo pro café da tarde?'
+}
+else if (currentHour >= 11 && currentHour <= 14) {
+  message = 'Hora do almoço!!!'
+}
+else {
+  message = 'Hmmm, cheiro de café recém-passado'
+}
+
+console.log(message)
+
+let statusPessoa = 'reprovado'
+
+switch (statusPessoa) {
+
+  case 'aprovada':
+    console.log('Parabéns, você está no grupo de pessoas aprovadas!');
+
+    break
+
+  case 'lista':
+    console.log('Você está na nossa lista de espera.')
+    break
+  case 'reprovado':
+    console.log('Infelizmente, você reprovou.')
+    break
+
+  default:
+    console.log('Informação incorreta.')
+
+}
+
+console.log()
---
+++ fundamentos/secao-introducao-a-javascript/primeiros-passos/script2.js
@@ -0,0 +1,19 @@
+let a = 5
+let b = 8
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+
+let num1 = 10
+let num2 = 20
+
+if (num1 > num2) {
+  console.log(`o numero ${num1} é maior que o numero ${num2}`)
+}
+else {
+  console.log(`o numero ${num2} é maior que o numero ${num1}`)
+}
+
