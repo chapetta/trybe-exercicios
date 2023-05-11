69b289431fad39b2b99000666f889457b4612850
chapetta <cha.petta@hotmail.com>
2023-05-11 18:32:22 -0300

    exercicios de if e else

---
+++ fundamentos/secao-introducao-a-javascript/primeiros-passos/script.js
@@ -0,0 +1,11 @@
+let nota = 85
+
+if (nota > 80) {
+  console.log("parabens, voce foi aprovado")
+}
+else if (nota < 80 && nota > 60 ) {
+  console.log('Você está na nossa lista de espera')
+}
+else {
+  console.log('infelizmente voce reprovou')
+}
